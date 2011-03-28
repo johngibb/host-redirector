05ab7da996456ccba1538eda05cd6f0fcb88a366
John Gibb <johngibb@gmail.com>
2011-03-28 01:12:03 -0700

    add tests based on hosts

--- test/host_redirector_test.rb
+++ test/host_redirector_test.rb
@@ -3,8 +3,26 @@ require 'test_helper'
 class HostRedirectorTest < ActionController::TestCase
   tests MyController
   
+  setup do
+    @old_host = get_config_host
+    set_config_host "localhost"
+  end
+  
+  teardown do
+    set_config_host @old_host
+  end
+  
   test "should get index" do
+    request.host = "localhost"
+    
     get :index
     assert_response :success
+  end
+  
+  test "should redirect" do
+    request.host = "127.0.0.1"
+
+    get :index
+    assert_redirected_to "http://localhost/"
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,15 @@ ENV["RAILS_ENV"] = "test"
 
 require "rails_app/config/environment"
 require "rails/test_help"
+
+def get_config_host
+  config.host if config.respond_to? :host
+end
+
+def set_config_host(host)
+  config.host = host
+end
+
+def config
+  RailsApp::Application.config
+end
