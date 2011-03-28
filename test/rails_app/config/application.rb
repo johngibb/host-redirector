ab7d57aafdea07b19feaadd6e7cdf2a1f9ce28e3
John Gibb <johngibb@gmail.com>
2011-03-28 00:16:42 -0700

    add skeleton rails app for testing

---
+++ test/rails_app/config/application.rb
@@ -0,0 +1,14 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# If you have a Gemfile, require the gems listed there, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env) if defined?(Bundler)
+
+module RailsApp
+  class Application < Rails::Application
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
+  end
+end
---
+++ test/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '641a495bc67e2b8c7410c569ed0e8876b71319ac77693b8bb04a306a4a5dabab09dfbe149890cb8e07339ed50aeec88bac482a8e6e989184108928a52ae9bc8a'
